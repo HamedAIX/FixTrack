95483e87a8a02273df1b8d230224a43fac9600c1
Hamed Al-Areqi <hamedlite2004@gmail.com>
2026-02-26 16:05:07 +0300

    Change server port to 5000 and update MONGO_URI

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/fixtrack";
+const MONGO_URI = process.env.MONGO_URI;
 
 async function startServer() {
   try {
